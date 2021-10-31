f0f74712929fe3bf5ce92a2cb7ba774b1994717b
jeongdev <wkdgowjdpo@gmail.com>
2021-10-31 15:26:44 +0900

    1~3번 문제

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ImportConponent from './R003_ImportConponent';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Start React!</h1>
+      <p>리액트 처음 시작하기</p>
+      <p>conponent사용하기 </p>
+      <ImportConponent></ImportConponent>
     </div>
   );
 }
