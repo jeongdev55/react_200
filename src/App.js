58aee15a2126d8b9ff90c3d7d46d9edd1ec57527
jeongdev <wkdgowjdpo@gmail.com>
2021-11-05 00:28:21 +0900

    ~28번

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
-import PropsNode from './R023_PropsNode'
+import PureComponentClass from './R028_PureComponentClass'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <PropsNode>
-        <span>node from App.js</span>
-      </PropsNode>
+      <PureComponentClass/>
     </div>
   );
 }
