2dfd14c333ee0570f75d2453351fbd6bd88cd3a9
jeongdev <wkdgowjdpo@gmail.com>
2021-11-08 23:26:51 +0900

    ~38번

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-// import './App.css';
-import ReturnMap from './R033_ReturnMap'
+import './App.css';
+import ReactstrapButtonGroup from './R038_ReactstrapButtonGroup'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReturnMap/>
+      <ReactstrapButtonGroup/>
     </div>
   );
 }
