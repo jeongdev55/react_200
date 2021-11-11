854fc97eabec82ee656da0c1d475b92991448950
jeongdev <wkdgowjdpo@gmail.com>
2021-11-11 20:53:54 +0900

    ~45번

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
-import ReactstrapButtonGroup from './R038_ReactstrapButtonGroup'
+// import './App.css';
+import InputGroup from './R045_ReactstrapInputGroup'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,8 +8,7 @@ function App() {
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactstrapButtonGroup/>
-      
+      <InputGroup/>
     </div>
   );
 }
